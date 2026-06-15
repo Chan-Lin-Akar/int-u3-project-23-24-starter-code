207181f35f295d3b5eaa0116e95f561dbdce4cca
Chan-Lin-Akar <chanlinn.arkar.dawei@gmail.com>
2026-06-14 21:18:41 -0400

    DoneUnit3

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
 // Declare variables below to save the different divs of your story.
+let optionTwoBtn = document.querySelector('.option-two');
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let opening = document.querySelector('.story-opening');
+let butHome = document.querySelector('.buttons-home');
+let tit = document.querySelector('.title');
+
+optionTwoBtn.addEventListener('click', function() {
+    optionTwoScreen.style.display = 'block';
+    opening.style.display = 'none';
+    butHome.style.display = 'none';
+    tit.style.display = 'none';
+
+});
+
+let nept = document.querySelector('.neptnue');
+let optionTwoEnd = document.querySelector('.option-two-end');
+
+nept.addEventListener('click', function(){
+    optionTwoEnd.style.display = 'block';
+    optionTwoScreen.style.display = 'none';
+
+});
+
+let again = document.querySelector('.startAgain');
+
+again.addEventListener('click', function() {
+    opening.style.display = 'block';
+    butHome.style.display = 'block';
+    tit.style.display = 'block';
+    optionTwoEnd.style.display = 'none';
+    
+
+});
 
 
+let optionOneBtn = document.querySelector('.option-one');
+let optionOneScreen = document.querySelector('.option-one-screen');
 
+optionOneBtn.addEventListener('click', function() {
+    optionOneScreen.style.display = 'block';
+    opening.style.display = 'none';
+    butHome.style.display = 'none';
+    tit.style.display = 'none';
 
+});
 
+let nit = document.querySelector('.nitrogen');
+let optionOneEnd = document.querySelector('.option-one-end');
 
+nit.addEventListener('click', function() {
+    optionOneScreen.style.display = 'none';
+    optionOneEnd.style.display = 'block';
+
+});
+
+let again2 = document.querySelector('.startAgain2');
+
+again.addEventListener('click', function() {
+    opening.style.display = 'block';
+    butHome.style.display = 'block';
+    tit.style.display = 'block';
+    optionOneEnd.style.display = 'none';
+    
+
+});
 
 
 
